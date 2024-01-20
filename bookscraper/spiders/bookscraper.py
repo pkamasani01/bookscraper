cfae2544f35926dd6daa359fdc106a459fa3de25
premk <premk@HABIBI>
2024-01-19 19:26:28 -0600

    Add project files.

---
+++ bookscraper/spiders/bookscraper.py
@@ -0,0 +1,63 @@
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from bookscraper.items import BookscraperItem
+import os
+from csv import writer
+
+class BookScraper(CrawlSpider):
+    name = "bookscraper"
+    start_urls = ["http://books.toscrape.com/"]
+    
+    rules = (
+        Rule(LinkExtractor(restrict_css=".nav-list > li > ul > li > a"),follow = True),
+        Rule(LinkExtractor(restrict_css=".product_pod > h3 > a"),callback = "parse_book")
+        )
+
+    def parse_book(self, response):
+        book_item = BookscraperItem()
+        book_item["image_url"] = response.urljoin(response.css(".item.active > img::attr(src)").get())
+        book_item["title"] = response.css(".col-sm-6.product_main > h1::text").get()
+        book_item["upc"] = response.css(".table.table-striped > tr:nth-child(1) > td::text").get()
+        book_item["url"] = response.url
+        book_item["price"] = response.css(".table.table-striped > tr:nth-child(4) > td::text").get()
+        book_item["availability"] = response.css(".table.table-striped > tr:nth-child(6) > td::text").get()
+        book_item["reviews"] = response.css(".table.table-striped > tr:nth-child(7) > td::text").get()
+        self.Save_to_file(
+            book_item["image_url"],
+            book_item["title"], 
+            book_item["price"],
+            book_item["upc"], 
+            book_item["url"],
+            book_item["availability"],
+            book_item["reviews"]
+            )
+        
+        return book_item
+    
+    def Save_to_file(self, image_url, title, price, upc, url, availability, reviews):
+        self.price = price
+        self.image_url = image_url
+        self.title = title
+        self.upc = upc
+        self.url = url
+        self.availability = availability
+        self.reviews = reviews
+        if os.path.isfile("data.csv"):
+            with open('data.csv', 'a') as f_object:
+                writer_object = writer(f_object)
+                # Pass the list as an argument into
+                # the writerow()
+                writer_object.writerow([self.image_url, self.title, self.price, self.upc, self.url, self.availability, self.reviews])
+                # Close the file object
+                f_object.close()
+        else:
+            with open('data.csv', 'w') as f_object:
+                writer_object = writer(f_object)
+                
+                # Pass the list as an argument into
+                # the writerow()
+                writer_object.writerow(["image_url", "title", "price", "upc", "url", "availability", "reviews"])
+                writer_object.writerow([self.image_url, self.title, self.price, self.upc, self.url, self.availability, self.reviews])
+                # Close the file object
+                f_object.close()
+        
